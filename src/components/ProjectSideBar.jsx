0ed8dfc351a89c9d565180ab874e32aef1e75661
Furkan TÜRKER <furkantrkr13@gmail.com>
2023-10-28 18:17:25 +0300

    project starter

---
+++ src/components/ProjectSideBar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectSideBar() {
+  return (
+    <aside>
+      <h2>YOUR PROJECTS</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
